a863c0306b95559618bcce928a141d44f23a9bfb
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-07-22 19:46:28 -0400

    AOS

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -9,6 +9,9 @@ import auto from 'public/auto.jpg';
 import soapayuda from 'public/soapayuda .png';
 
 import CarruselImages from 'components/carrusel';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const navItems = [
     { linkText: '¿Quiénes somos?', href: '#quienesomos' },
@@ -33,6 +36,13 @@ const compraaqui = [
 ];
 
 export default function Page() {
+
+    useEffect(() => {
+        AOS.init({
+          duration: 1200,
+        });
+      }, []);
+
     const [currentImage, setCurrentImage] = useState(null);
     const [showInfoBox, setShowInfoBox] = useState(false);
     const [infoContent, setInfoContent] = useState('');
@@ -52,8 +62,8 @@ export default function Page() {
 
     return (
 
-        <main className="text-center">
-            <nav className="absolute top-0 left-0 flex items-center justify-center pt-2 md:pt-4 pb-2 md:pb-4 z-10 w-[100%] bg-white bg-opacity-50">
+        <main className="text-center ">
+            <nav data-aos="fade-down" className="absolute top-0 left-0 flex items-center justify-center pt-2 md:pt-4 pb-2 md:pb-4 z-10 w-[100%] bg-white bg-opacity-50">
                 <Image src={soapayuda} alt="SOAP Ayuda" className="hidden sm:inline-block sm:w-[68px] md:w-[144px] mr-8" />
                 {!!navItems?.length && (
             <ul className="flex gap-2 sm:gap-4 md:gap-5">
@@ -74,17 +84,17 @@ export default function Page() {
             </section>
 
             <section className="text-center max-w-screen-lg mx-auto">
-                <h2 id="quienesomos" className="scroll-m-header text-3xl md:text-5xl font-bold text-[#1e5db2] mt-10 md:mt-16 mb-10 md:mb-16"><Image src={corazon} alt="" unoptimized className="inline-block mr-2 w-[90px] md:w-[150px]" />¿Quiénes somos?</h2>
-                <p className="text-1xl md:text-2xl font-medium text-black m-7 leading-6 md:leading-10"><span className="text-[#1e5db2] font-bold">SOAP</span> <span className="text-[#22beeb] font-bold">AYUDA</span> nace bajo el concepto de ayudar a diversas instituciones y/o fundaciones que mediante la venta del Seguro Obligatorio puedan recibir donaciones con el objetivo de continuar con sus labores sociales.</p>
-                <p className="text-1xl md:text-2xl font-medium text-black m-7 leading-6 md:leading-10"><span className="text-[#1e5db2] font-bold">SOAP</span> <span className="text-[#22beeb] font-bold">AYUDA</span> proviene de una red de venta que trabaja para prestar el mejor servicio hace más de 20 años con casa matriz en la ciudad de Valparaíso.</p>
-                <p className="text-1xl md:text-2xl font-medium text-black m-7 leading-6 md:leading-10"><span className="text-[#1e5db2] font-bold">Nuestra VISIÓN es</span> <span className="text-[#22beeb] font-bold">AYUDAR</span> a más instituciónes y/o Fundaciones mediante la venta online del Seguro Obligatorio <span className="text-[#1e5db2] font-bold">SOAP</span> <span className="text-[#22beeb] font-bold">AYUDA</span>.</p>
-                <p className="text-3xl md:text-5xl font-serif font-bold italic mt-16 mr-20 mb-20 ml-20"><span className="text-[#1e5db2]">Súmate </span><span className="text-[#22beeb]">a esta gran labor</span></p>
-                <h2 id="ayudasocial" className="scroll-m-header text-3xl md:text-5xl font-bold text-[#1e5db2] mt-16 md:mt-28 mb-10 md:mb-16"><Image src={corazon} alt="" unoptimized className="inline-block mr-2 w-[90px] md:w-[150px]" />Ayuda Social</h2>
-                <p className="text-1xl md:text-2xl font-medium text-black m-7 leading-6 md:leading-10">Actualmente <span className="text-[#1e5db2] font-bold">SOAP</span> <span className="text-[#22beeb] font-bold">AYUDA</span> está presente con convenios en diferentes Instituciones y/o Fundaciones de <span className="text-[#22beeb] font-bold">ámbito social</span>.</p>
+                <h2 data-aos="fade-up" id="quienesomos" className="scroll-m-header text-3xl md:text-5xl font-bold text-[#1e5db2] mt-10 md:mt-16 mb-10 md:mb-16"><Image src={corazon} alt="" unoptimized className="inline-block mr-2 w-[90px] md:w-[150px]" />¿Quiénes somos?</h2>
+                <p data-aos="fade-up" className="text-1xl md:text-2xl font-medium text-black m-7 leading-6 md:leading-10"><span className="text-[#1e5db2] font-bold">SOAP</span> <span className="text-[#22beeb] font-bold">AYUDA</span> nace bajo el concepto de ayudar a diversas instituciones y/o fundaciones que mediante la venta del Seguro Obligatorio puedan recibir donaciones con el objetivo de continuar con sus labores sociales.</p>
+                <p data-aos="fade-up" className="text-1xl md:text-2xl font-medium text-black m-7 leading-6 md:leading-10"><span className="text-[#1e5db2] font-bold">SOAP</span> <span className="text-[#22beeb] font-bold">AYUDA</span> proviene de una red de venta que trabaja para prestar el mejor servicio hace más de 20 años con casa matriz en la ciudad de Valparaíso.</p>
+                <p data-aos="fade-up" className="text-1xl md:text-2xl font-medium text-black m-7 leading-6 md:leading-10"><span className="text-[#1e5db2] font-bold">Nuestra VISIÓN es</span> <span className="text-[#22beeb] font-bold">AYUDAR</span> a más instituciónes y/o Fundaciones mediante la venta online del Seguro Obligatorio <span className="text-[#1e5db2] font-bold">SOAP</span> <span className="text-[#22beeb] font-bold">AYUDA</span>.</p>
+                <p data-aos="fade-up" className="text-3xl md:text-5xl font-serif font-bold italic mt-16 mr-20 mb-20 ml-20"><span className="text-[#1e5db2]">Súmate </span><span className="text-[#22beeb]">a esta gran labor</span></p>
+                <h2 data-aos="fade-up" id="ayudasocial" className="scroll-m-header text-3xl md:text-5xl font-bold text-[#1e5db2] mt-16 md:mt-28 mb-10 md:mb-16"><Image src={corazon} alt="" unoptimized className="inline-block mr-2 w-[90px] md:w-[150px]" />Ayuda Social</h2>
+                <p data-aos="fade-up" className="text-1xl md:text-2xl font-medium text-black m-7 leading-6 md:leading-10">Actualmente <span className="text-[#1e5db2] font-bold">SOAP</span> <span className="text-[#22beeb] font-bold">AYUDA</span> está presente con convenios en diferentes Instituciones y/o Fundaciones de <span className="text-[#22beeb] font-bold">ámbito social</span>.</p>
             </section>
 
             <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-4 mt-16 md:mt-28 mb-10 md:mb-16 wx-auto duration-300 ease-in-out bg-[#22beeb] p-8 max-w-screen-xl mx-auto rounded-none sm:rounded-none md:rounded-none lg:rounded-none xl:rounded-xl ">
-                {compraaqui.map((item, index) => (<div key={index} className="px-20 py-10 sm:p-10 md:p-10 lg:p-4 duration-300 ease-in-out bg-white rounded-lg">
+                {compraaqui.map((item, index) => (<div data-aos="fade-down" key={index} className="px-20 py-10 sm:p-10 md:p-10 lg:p-4 duration-300 ease-in-out bg-white rounded-lg">
                     <p className="cursor-pointer rounded-lg overflow-hidden hover:border-[#1e5db2] border-2 border-transparent duration-300 ease-in-out" onClick={() => openInfoBox(item.info, item.image)}><Image src={item.image} alt="" unoptimized className="mx-auto" /></p>
                     <p className="mt-8 nowrap"><Image src={mutualdeseguros} alt="" unoptimized className="inline-block w-8 mr-2" /> <Link href={`/compraaquimut${item.fundacion}`} className="inline-block rounded-full px-4 pt-1 pb-2 bg-[linear-gradient(#22beeb,#1e5db2)] text-white text-lg md:text-xl no-underline font-medium" >Compra aquí</Link></p>
                     <p className="mt-4 nowrap"><Image src={bcibanco} alt="" unoptimized className="inline-block w-8 mr-2" /> <Link href={`/compraaquibci${item.fundacion}`} className="inline-block rounded-full px-4 pt-1 pb-2 bg-[linear-gradient(#22beeb,#1e5db2)] text-white text-lg md:text-xl no-underline font-medium" >Compra aquí</Link></p>
@@ -102,20 +112,20 @@ export default function Page() {
             )}
 
             <section className="text-center max-w-screen-lg mx-auto">
-                <h2 className="text-3xl md:text-5xl font-bold text-[#22beeb] mt-16 md:mt-28 mb-10 md:mb-16"><Image src={corazon} alt="" unoptimized className="inline-block mr-2 w-[90px] md:w-[150px]" />¿Cómo funciona?</h2>
-                <p className="text-left text-2xl md:text-3xl font-bold text-[#1e5db2] m-7 ">1.- Haga click en el botón de compra de su elección</p>
-                <p className="text-left text-2xl md:text-3xl font-bold text-[#22beeb] m-7 ">2.- Complete el Formulario</p>
-                <p className="text-left text-2xl md:text-3xl font-bold text-[#1e5db2] m-7 ">3.- Seleccione forma de pago</p>
-                <p className="text-left text-2xl md:text-3xl font-bold text-[#22beeb] m-7 ">4.- Hecho el pago ya has donado a la institución elegida</p>
+                <h2 data-aos="fade-up" className="text-3xl md:text-5xl font-bold text-[#22beeb] mt-16 md:mt-28 mb-10 md:mb-16"><Image src={corazon} alt="" unoptimized className="inline-block mr-2 w-[90px] md:w-[150px]" />¿Cómo funciona?</h2>
+                <p data-aos="fade-up" className="text-left text-2xl md:text-3xl font-bold text-[#1e5db2] m-7 ">1.- Haga click en el botón de compra de su elección</p>
+                <p data-aos="fade-up" className="text-left text-2xl md:text-3xl font-bold text-[#22beeb] m-7 ">2.- Complete el Formulario</p>
+                <p data-aos="fade-up" className="text-left text-2xl md:text-3xl font-bold text-[#1e5db2] m-7 ">3.- Seleccione forma de pago</p>
+                <p data-aos="fade-up" className="text-left text-2xl md:text-3xl font-bold text-[#22beeb] m-7 ">4.- Hecho el pago ya has donado a la institución elegida</p>
             </section>
 
             <section className="text-center max-w-screen-xl mx-auto">
-                <h2 id="tuaporte" className="scroll-m-header text-3xl md:text-5xl font-bold text-[#1e5db2] mt-16 md:mt-28 mb-10 md:mb-16"><Image src={corazon} alt="" unoptimized className="inline-block mr-2 w-[90px] md:w-[150px]" />Tu aporte SOAP</h2>
+                <h2 data-aos="fade-up" id="tuaporte" className="scroll-m-header text-3xl md:text-5xl font-bold text-[#1e5db2] mt-16 md:mt-28 mb-10 md:mb-16"><Image src={corazon} alt="" unoptimized className="inline-block mr-2 w-[90px] md:w-[150px]" />Tu aporte SOAP</h2>
                 <CarruselImages />
             </section>
 
             <section className="text-center max-w-screen-md mx-auto mb-0 sm:mb-0 md:mb-10 lg:mb-10">
-                <h2 id="contacto" className="scroll-m-header text-3xl md:text-5xl font-bold text-[#1e5db2] mt-16 md:mt-28 mb-10 md:mb-16"><Image src={corazon} alt="" unoptimized className="inline-block mr-2 w-[90px] md:w-[150px]" />Contacto</h2>
+                <h2 data-aos="fade-up" id="contacto" className="scroll-m-header text-3xl md:text-5xl font-bold text-[#1e5db2] mt-16 md:mt-28 mb-10 md:mb-16"><Image src={corazon} alt="" unoptimized className="inline-block mr-2 w-[90px] md:w-[150px]" />Contacto</h2>
                 {/*<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
                     <div className="p-4">
                         <div className="flex flex-col justify-center h-full">
@@ -139,7 +149,7 @@ export default function Page() {
                         <p className="mt-2 md:mt-4 font-bold text-socialBlue text-base md:text-lg">(32) 2233491</p>
                         </div>
                     </div>*/}
-                    <div className="pt-4 pr-4 pb-10 pl-4 bg-[#ddd] rounded-none sm:rounded-none md:rounded-xl lg:rounded-xl">
+                    <div data-aos="fade-down" className="pt-4 pr-4 pb-10 pl-4 bg-[#ddd] rounded-none sm:rounded-none md:rounded-xl lg:rounded-xl">
 
                         <form action="https://api.web3forms.com/submit" method="POST">
                             <input type="hidden" name="access_key" value="2211d83c-52ec-4484-a609-6fd57c3c47f1" />
