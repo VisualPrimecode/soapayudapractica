5e44b0f29b6103139afb14f8b7b4bf5a36bb0aaa
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-09-04 10:35:46 -0400

    fdfsd

--- app/layout.jsx
+++ app/layout.jsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
         };
     }, []);
     return (
-        <html lang="en">
+        <html lang="en" className={` scroll-smooth `}>
             <head>
                 <title>{metadata.title.default}</title>
             </head>
